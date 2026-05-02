10ec4b3ed0597f8b2583af9e1d891cac150d9061
John Prichett <jdp9705@nyu.edu>
2026-05-02 02:32:20 +0000

    Remove sensor/location listener references from Reporter.kt

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
@@ -1,21 +1,4 @@
 package com.example.giftcardsite.api.model
 
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.location.Location
-import android.location.LocationListener
-
-class Reporter(private val token: String) : LocationListener, SensorEventListener{
-    override fun onLocationChanged(location: Location) {
-
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-
-    }
+class Reporter(private val token: String) {
 }
